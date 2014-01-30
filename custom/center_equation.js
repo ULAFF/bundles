5006c3f2275bbc0ba3b13d6c9b1c61659d3c1126
rosatamsen <drrt@cs.utexas.edu>
2014-01-30 14:27:03 -0600

    first commit

---
+++ custom/center_equation.js
@@ -0,0 +1,13 @@
+MathJax.Hub.Config({
+    TeX: {
+        extensions: ["AMSmath.js"]
+    },
+    tex2jax: {
+        inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+        displayMath: [ ['$$','$$'], ["\\[","\\]"] ]
+    },
+    displayAlign: 'center', // Change this to 'center' to center equations.
+    "HTML-CSS": {
+        styles: {'.MathJax_Display': {"margin": 4}}
+    }
+});
